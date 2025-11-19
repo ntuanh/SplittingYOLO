da6b6acd9c65e7a486e12d1ead4416d60c0a63c1
ntuanh <ntuanh2705@gmail.com>
2025-11-19 16:07:52 +0700

    feat : measure RAM , VRAM and time

--- SplitYOLO/src/origin_model.py
+++ SplitYOLO/src/origin_model.py
@@ -1,105 +1,105 @@
-    from ultralytics import YOLO
-    import cv2
-    import time
-    import psutil
-    import torch , yaml , tracemalloc
-    from Utils import get_ram , get_vram , reset_vram
-    from ultralytics.nn.tasks import DetectionModel
-    from ultralytics.utils.plotting import Annotator, colors
-    from ultralytics.models.yolo.detect import DetectionPredictor
-    import torchvision.transforms as T
-    from PIL import Image
+from ultralytics import YOLO
+import cv2
+import time
+import psutil
+import torch , yaml , tracemalloc
+from Utils import get_ram , get_vram , reset_vram
+from ultralytics.nn.tasks import DetectionModel
+from ultralytics.utils.plotting import Annotator, colors
+from ultralytics.models.yolo.detect import DetectionPredictor
+import torchvision.transforms as T
+from PIL import Image
 
-    # pick device
-    # device = "cpu"
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"[DEVICE] { device}")
+# pick device
+# device = "cpu"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"[DEVICE] { device}")
 
-    ram_before = get_ram()
-    vram_before =  reset_vram()
-    t0 = time.time()
+ram_before = get_ram()
+vram_before =  reset_vram()
+t0 = time.time()
 
-    cfg = yaml.safe_load(open('./cfg/yolo11n.yaml', 'r', encoding='utf-8'))
-    model = DetectionModel(cfg, verbose=False).to(device)
+cfg = yaml.safe_load(open('./cfg/yolo11n.yaml', 'r', encoding='utf-8'))
+model = DetectionModel(cfg, verbose=False).to(device)
 
-    t1 = time.time()
-    ram_after = get_ram()
-    vram_after = get_vram()
-    print(f"[Architecture] Time: {t1 - t0:.4f} s")
-    print(f"[Architecture] RAM used:  {(ram_after - ram_before):.4f} MB")
-    print(f"[Architecture] VRAM used: {(vram_after):.4f} MB")
+t1 = time.time()
+ram_after = get_ram()
+vram_after = get_vram()
+print(f"[Architecture] Time: {t1 - t0:.4f} s")
+print(f"[Architecture] RAM used:  {(ram_after - ram_before):.4f} MB")
+print(f"[Architecture] VRAM used: {(vram_after):.4f} MB")
 
-    # ==========================
-    # 2. LOAD WEIGHTS
-    # ==========================
-    ram_before = get_ram()
-    vram_before = reset_vram()
-    t0 = time.time()
+# ==========================
+# 2. LOAD WEIGHTS
+# ==========================
+ram_before = get_ram()
+vram_before = reset_vram()
+t0 = time.time()
 
-    state_dict = torch.load('./yolo11weights.pt', map_location=device, weights_only=True)
-    model.load_state_dict(state_dict, strict=False)
-    model.eval()
+state_dict = torch.load('./yolo11weights.pt', map_location=device, weights_only=True)
+model.load_state_dict(state_dict, strict=False)
+model.eval()
 
-    t1 = time.time()
-    ram_after = get_ram()
-    vram_after = reset_vram()
+t1 = time.time()
+ram_after = get_ram()
+vram_after = reset_vram()
 
-    print(f"[Weights] Time: {t1 - t0:.4f} s")
-    print(f"[Weights] RAM used:  {(ram_after - ram_before):.4f} MB")
-    print(f"[Weights] VRAM used: {(vram_after - vram_before):.4f} MB")
+print(f"[Weights] Time: {t1 - t0:.4f} s")
+print(f"[Weights] RAM used:  {(ram_after - ram_before):.4f} MB")
+print(f"[Weights] VRAM used: {(vram_after - vram_before):.4f} MB")
 
-    # ============================================================
-    # forward function
-    # ============================================================
-    img = Image.open('./data/image.png').convert('RGB')
-    transform = T.Compose([
-        T.Resize((640, 640)),
-        T.ToTensor(),
-    ])
-    x = transform(img).unsqueeze(0)
-    def forward(model, x):
-        y = {}             # store outputs of layers
-        current_x = x      # input image
+# ============================================================
+# forward function
+# ============================================================
+img = Image.open('./data/image.png').convert('RGB')
+transform = T.Compose([
+    T.Resize((640, 640)),
+    T.ToTensor(),
+])
+x = transform(img).unsqueeze(0)
+def forward(model, x):
+    y = {}             # store outputs of layers
+    current_x = x      # input image
 
-        for layer in model.model:
-            if isinstance(layer.f, int):
-                x_in = current_x if layer.f == -1 else y[layer.f]
-            else:
-                x_in = [(current_x if j == -1 else y[j]) for j in layer.f]
+    for layer in model.model:
+        if isinstance(layer.f, int):
+            x_in = current_x if layer.f == -1 else y[layer.f]
+        else:
+            x_in = [(current_x if j == -1 else y[j]) for j in layer.f]
 
-            current_x = layer(x_in)
+        current_x = layer(x_in)
 
-            y[layer.i] = current_x
+        y[layer.i] = current_x
 
-        return current_x
+    return current_x
 
 
-    # 3.2 Check cuda available
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # model = model.to(device)
-    x = x.to(device)
-    # print(f"[DEVICE] : {device}")
+# 3.2 Check cuda available
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model = model.to(device)
+x = x.to(device)
+# print(f"[DEVICE] : {device}")
 
-    # ============================================================
-    # 4. FORWARD TAIL
-    # ============================================================
+# ============================================================
+# 4. FORWARD TAIL
+# ============================================================
 
-    ram_before = get_ram()
-    vram_before = reset_vram()
-    t0 = time.time()
+ram_before = get_ram()
+vram_before = reset_vram()
+t0 = time.time()
 
-    with torch.inference_mode():
-        preds = forward(model,x)
+with torch.inference_mode():
+    preds = forward(model,x)
 
-    t1 = time.time()
-    ram_after = get_ram()
-    vram_after = get_vram()
+t1 = time.time()
+ram_after = get_ram()
+vram_after = get_vram()
 
-    print(f"[Forward] Time: {t1 - t0:.4f} s")
-    print(f"[Forward] RAM used:  {(ram_after - ram_before):.4f} MB")
-    print(f"[Forward] VRAM used: {(vram_after - vram_before):.4f} MB")
+print(f"[Forward] Time: {t1 - t0:.4f} s")
+print(f"[Forward] RAM used:  {(ram_after - ram_before):.4f} MB")
+print(f"[Forward] VRAM used: {(vram_after - vram_before):.4f} MB")
 
-    print("\nInference done.")
+print("\nInference done.")
 
 # # ------------------------------
 # # Load model
